0df3bd4cec54a974fc14e692bbeaab5d536b1af7
Max Dayton <daytonmaximus@gmail.com>
2022-12-06 12:57:22 -1000

    working on it

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,3 +1,26 @@
+enum Action {
+    ROCK,
+    PAPER,
+    SCISSORS,
+}
+
+struct Move {
+    action: Action,
+    points: u32,
+}
+
+struct Round {
+    opponent_move: Move,
+    player_move: Move,
+    result: u32,
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn calculate_round_result(round: Round) -> u32 {
+    let mut result: u32 = 0;
+
+    return result;
 }

85ed5f0e7d50651c0700bbd1e862152ef01c9c75
Max Dayton <daytonmaximus@gmail.com>
2022-12-10 20:32:09 -1000

    working on day 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,26 +1,51 @@
+#[derive(Debug)]
 enum Action {
-    ROCK,
-    PAPER,
-    SCISSORS,
+    ROCK = 1,
+    PAPER = 2,
+    SCISSORS = 3,
 }
 
-struct Move {
-    action: Action,
-    points: u32,
+impl Action {
+    pub fn value(&self) -> u8 {
+        match *self {
+            Action::ROCK => 1,
+            Action::PAPER => 2,
+            Action::SCISSORS => 3,
+        }
+    }
+
+    pub fn opponent_code(&self) -> String {
+        match *self {
+            Action::ROCK => "A".to_string(),
+            Action::PAPER => "A".to_string(),
+            Action::SCISSORS => "C".to_string(),
+        }
+    }
 }
 
 struct Round {
-    opponent_move: Move,
-    player_move: Move,
-    result: u32,
+    opponent_action: Action,
+    player_action: Action,
+    result: u8,
 }
 
 fn main() {
-    println!("Hello, world!");
+    calculate_round("B".to_string(), "Z".to_string());
 }
 
-fn calculate_round_result(round: Round) -> u32 {
-    let mut result: u32 = 0;
+fn calculate_round(encrypted_opponent_action: String, encrypted_player_action: String) -> Round {
+    let round: Round;
 
-    return result;
+    println!("round.opponent_action: {:?}", round.opponent_action);
+    println!(
+        "round.opponent_action.value(): {:?}",
+        round.opponent_action.value()
+    );
+    println!("round.player_action: {:?}", round.player_action);
+    println!(
+        "round.player_action.value(): {:?}",
+        round.player_action.value()
+    );
+    println!("round.result: {}", round.result);
+    return round;
 }
